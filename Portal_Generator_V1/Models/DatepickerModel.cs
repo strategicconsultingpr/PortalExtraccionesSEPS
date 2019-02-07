ed5f01a6d878e3177cbc0df748f87acdbe638bfd
Alexie Ortiz Falu <aortiz@strategicconsultingpr.com>
2019-02-07 11:30:51 -0400

    Se comento seccion de leer status de la creacion de nuva tabla access, ya que no se utilizara

--- Portal_Generator_V1/Models/DatepickerModel.cs
+++ Portal_Generator_V1/Models/DatepickerModel.cs
@@ -182,7 +182,7 @@ namespace Portal_Generator_V1.Models
             String result = "", resultFileName = "";
 
             sql = "SELECT [Value] FROM [dbo].[SEPS-DB_Parameters] WHERE[Parameter] = 'status'";
-            sqlFileName = "SELECT [Value] FROM [dbo].[SEPS-DB_Parameters] WHERE[Parameter] = 'filename'";
+         //   sqlFileName = "SELECT [Value] FROM [dbo].[SEPS-DB_Parameters] WHERE[Parameter] = 'filename'";
 
             sqlCnn = new SqlConnection(ConfigurationManager.ConnectionStrings[2].ConnectionString);
 
@@ -196,20 +196,21 @@ namespace Portal_Generator_V1.Models
             sqlReader.Close();
             sqlCmd.Dispose();
 
-            CmdFileName = new SqlCommand(sqlFileName, sqlCnn);
-            SqlDataReader sqlReaderFile = CmdFileName.ExecuteReader();
-            while (sqlReaderFile.Read())
-            {
-                resultFileName = sqlReaderFile.GetValue(0).ToString();
-            }
-            sqlReaderFile.Close();
-            CmdFileName.Dispose();
+            //CmdFileName = new SqlCommand(sqlFileName, sqlCnn);
+            //SqlDataReader sqlReaderFile = CmdFileName.ExecuteReader();
+            //while (sqlReaderFile.Read())
+            //{
+            //    resultFileName = sqlReaderFile.GetValue(0).ToString();
+            //}
+            //sqlReaderFile.Close();
+            //CmdFileName.Dispose();
 
 
 
             sqlCnn.Close();
 
-            if (result.Equals("1") && !resultFileName.Equals("0")) { value = true; }
+            //if (result.Equals("1") && !resultFileName.Equals("0")) { value = true; }
+            if (result.Equals("1")) { value = true; }
 
             return value;
         }
